e7e1567ec9dfebcb8b902ff36143dab2a33e2a02
nishant12 <tniranjan710@gmail.com>
2025-04-09 23:28:10 +0530

    delete product from cart when clicking delete link

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
-import { cart } from "../data/cart.js";
+import { cart, removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formateCurrency } from "./utils/money.js";
+
 
 let cartSummaryHtml = '';
 
@@ -41,7 +42,7 @@ cart.forEach((cartItem)=>{
                     <span class="update-quantity-link link-primary">
                       Update
                     </span>
-                    <span class="delete-quantity-link link-primary">
+                    <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
                       Delete
                     </span>
                   </div>
@@ -98,3 +99,12 @@ cart.forEach((cartItem)=>{
 });
 
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHtml;
+
+document.querySelectorAll('.js-delete-link')
+  .forEach((link)=>{
+    link.addEventListener('click', ()=>{
+      const productId = link.dataset.productId;
+      removeFromCart(productId);
+      console.log(cart);
+    })
+  })
