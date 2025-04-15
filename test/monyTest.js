19015f1739ee36b385ccfeacaf06f2592ac398e3
nishant12 <tniranjan710@gmail.com>
2025-04-16 01:05:12 +0530

    create automate test for fromate currency

---
+++ test/monyTest.js
@@ -0,0 +1,17 @@
+import { formateCurrency } from "../scripts/utils/money.js";
+
+console.log('test suit: formate Currency')
+
+if (formateCurrency(2095) === '20.95'){  // basic test cases
+  console.log('passed')
+} else {
+  console.log('failed')
+}
+
+console.log('rounds upto nearest cents')
+
+if (formateCurrency(2000.5)=== '20.01'){ // edge test cases
+  console.log('passed')
+} else{
+  console.log('shitt-filed')
+}
