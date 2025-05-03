468140698e76c2e44655100e955a990e97b3e9d6
nishant12 <tniranjan710@gmail.com>
2025-05-03 14:01:24 +0530

    added instuctionLink and warrentyLink to Electronics

--- data/products.js
+++ data/products.js
@@ -55,6 +55,24 @@ class Clothing extends Product {   // Using Inheritence
   }
 }
 
+class Electronics extends Product { //using inheritance
+  instructionsLink;
+  warrantyLink;
+
+  constructor(productDetails){
+    super(productDetails);
+    this.instructionsLink = productDetails.instructionsLink;
+    this.warrantyLink = productDetails.warrantyLink;
+  }
+
+  extraInfoHTML (){
+    // super.extraInfoHTML();
+    return `
+    <a href="${this.instructionsLink}" target="_blank">Instuctions</a>
+    <a href="${this.warrantyLink}" target="_blank">Warranty</a>`;
+  }
+}
+
 /*
 const date = new Date();
 console.log(date);
@@ -135,7 +153,10 @@ export const products = [
       "toaster",
       "kitchen",
       "appliances"
-    ]
+    ],
+    type: "electronics",
+    instructionsLink: "images/appliance-instructions.png",
+    warrantyLink: "images/appliance-warranty.png"
   },
   {
     id: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
@@ -320,7 +341,10 @@ export const products = [
       "water boiler",
       "appliances",
       "kitchen"
-    ]
+    ],
+    type: "electronics",
+    instructionsLink: "images/appliance-instructions.png",
+    warrantyLink: "images/appliance-warranty.png"
   },
   {
     id: "6b07d4e7-f540-454e-8a1e-363f25dbae7d",
@@ -625,7 +649,10 @@ export const products = [
       "coffeemakers",
       "kitchen",
       "appliances"
-    ]
+    ],
+    type: "electronics",
+    instructionsLink: "images/appliance-instructions.png",
+    warrantyLink: "images/appliance-warranty.png"
   },
   {
     id: "02e3a47e-dd68-467e-9f71-8bf6f723fdae",
@@ -685,7 +712,10 @@ export const products = [
       "food blenders",
       "kitchen",
       "appliances"
-    ]
+    ],
+    type: "electronics",
+    instructionsLink: "images/appliance-instructions.png",
+    warrantyLink: "images/appliance-warranty.png"
   },
   {
     id: "36c64692-677f-4f58-b5ec-0dc2cf109e27",
@@ -737,6 +767,8 @@ export const products = [
 ].map((productDetails)=>{
   if(productDetails.type === 'clothing'){
     return new Clothing(productDetails);
+  } else if(productDetails.type === 'electronics'){
+    return new Electronics(productDetails);
   }
   return new Product(productDetails);
 });
