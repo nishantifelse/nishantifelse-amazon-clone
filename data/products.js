a3207cb9526cc9dc7b07052b5a315d6d7e40b801
nishant12 <tniranjan710@gmail.com>
2025-05-08 15:06:31 +0530

    leod product form the backend

--- data/products.js
+++ data/products.js
@@ -93,6 +93,30 @@ logThis();
 logThis.call('hello');
 */
 
+export let products = [];
+
+export function loadProducts(fun){ 
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', ()=>{
+    products = JSON.parse(xhr.response).map((productDetails)=>{
+      if(productDetails.type === 'clothing'){
+        return new Clothing(productDetails);
+      } else if(productDetails.type === 'electronics'){
+        return new Electronics(productDetails);
+      }
+      return new Product(productDetails);
+    });
+    console.log('load products');
+    fun(); // callback function 
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send();
+}
+
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -774,3 +798,4 @@ export const products = [
   }
   return new Product(productDetails);
 });
+*/
