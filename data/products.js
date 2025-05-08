0fe76d87eeb137cab9e27055a9bdccfbdd6a5c84
nishant12 <tniranjan710@gmail.com>
2025-05-08 18:19:09 +0530

    convert load product from CHR to fetch

--- data/products.js
+++ data/products.js
@@ -95,6 +95,29 @@ logThis.call('hello');
 
 export let products = [];
 
+export function loadProductsFetch(){
+  const promise = fetch('https://supersimplebackend.dev/products').then((response)=>{
+    return response.json()
+  }).then((productData)=>{
+    products = productData.map((productDetails)=>{
+      if(productDetails.type === 'clothing'){
+        return new Clothing(productDetails);
+      } else if(productDetails.type === 'electronics'){
+        return new Electronics(productDetails);
+      }
+      return new Product(productDetails);
+    });
+    console.log('load products');
+    
+  });
+  return promise;
+}
+/*
+loadProductsFetch().then(()=>{
+  console.log('next step');
+});
+*/
+
 export function loadProducts(fun){ 
   const xhr = new XMLHttpRequest();
 
